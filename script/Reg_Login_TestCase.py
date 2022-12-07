cc5311fa3122f1d730cf842283e5a9af20bda7d7
G_jason <451595799@qq.com>
2022-12-07 22:19:19 +0800

    22222

--- script/Reg_Login_TestCase.py
+++ script/Reg_Login_TestCase.py
@@ -12,7 +12,7 @@ from parameterized import parameterized
 
 class RegLoginCase(unittest.TestCase):
     def setUp(self) -> None:
-        self.login_api = Login_API()  # 实例化注册登录模块xxxx
+        self.login_api = Login_API()  # 实例化注册登录模块gggg
         self.session = requests.Session()
         self.cookie = None
         warnings.simplefilter("ignore", ResourceWarning)
